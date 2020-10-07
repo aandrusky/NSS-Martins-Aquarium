6b184533308248403f76991783d3b8bb7ea0fef7
Alan Andrusky <alanandrusky@gmail.com>
2020-10-07 14:15:44 -0500

    added HTML from index.html to page fish.js

---
+++ scripts/Fish.js
@@ -0,0 +1,29 @@
+//this page and function builds a single fish object
+
+export const Fish = (fishObj) => {
+    return`
+    <section class="fish">
+                    <h2 class="fish__name>${fishObj.name}</h2>
+                    <img class="fish__image" src="${fishObj.image}"
+                        alt="">
+                    <h3 class="factHeading">Length:</h3>
+                    <div class="fish__length">
+                        <ul>
+                            <li>${fishObj.length}</li>
+                        </ul>
+                    </div>
+                    <h3 class="factHeading">Location:</h3>
+                    <div class="fish__location">
+                        <ul>
+                            <li>${fishObj.location}</li>
+                        </ul>
+                    </div>
+                    <h3 class="factHeading">Diet:</h3>
+                    <div class="fish__diet">
+                        <ul>
+                            <li>${fishObj.food}</li>
+                        </ul>
+                    </div>
+                </section>
+    `
+}
